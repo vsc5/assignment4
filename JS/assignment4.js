011167e7564795bb9efed408a51a9addf151651a
vsc5 <vsc5@pitt.edu>
2016-11-26 22:43:15 -0500

    added google links

--- JS/assignment4.js
+++ JS/assignment4.js
@@ -21,22 +21,42 @@ $(function(){
   });
 });
 
-$('.flexsearch-input').focus(function (){
-  var curString = $(this).val();
-  if(curString == '') {
-    $('#dropdown').empty();
-  }
-  $('#dropdown').slideDown();
-  $('p').click(function (){
-    var sel = $(this).text();
-    $('.flexsearch-input').val(sel);
-  });
-  $('.flexsearch-input').keypress(function (e){
-    $('#dropdown').empty();
-    curString = $(this).val() + String.fromCharCode(e.which);
-    $.each(search, function(k, v) {
-      if(v.toLowerCase().indexOf(curString.toLowerCase()) === 0){
-        $('#dropdown').append('<p>'+v+'</p>');
+$(document).ready(function(){
+  $('.flexsearch-input').focus(function (){
+    var curString = $(this).val();
+    if(curString == '') {
+      $('#dropdown').empty();
+    }
+    $('#dropdown').slideDown();
+    $('p').click(function (e){
+      e.preventDefault();
+      var sel = $(this).text();
+      $('.flexsearch-input').val(sel);
+    });
+    $('.flexsearch-input').keypress(function (e){
+      $('#dropdown').empty();
+      curString = $(this).val() + String.fromCharCode(e.which);
+      $.each(search, function(k, v) {
+        if(v.toLowerCase().indexOf(curString.toLowerCase()) === 0){
+          var url = "http://google.com/search?q=" + v;
+          $('#dropdown').append('<p><a>'+v+'</a></p>');
+          $('#dropdown > p:last-child > a').attr('href', url);
+          $('#dropdown > p:last-child > a').attr('target', '_blank');
+        }
+      });
+    });
+    $('.flexsearch-input').keydown(function (e){
+      $('#dropdown').empty();
+      if(e.keyCode === 8){
+        curString = $(this).val().slice(0,-1);
+        $.each(search, function(k, v) {
+          if(v.toLowerCase().indexOf(curString.toLowerCase()) === 0){
+            var url = "http://google.com/search?q=" + v;
+            $('#dropdown').append('<p><a>'+v+'</a></p>');
+            $('#dropdown > p:last-child > a').attr('href', url);
+            $('#dropdown > p:last-child > a').attr('target', '_blank');
+          }
+        });
       }
     });
   });
